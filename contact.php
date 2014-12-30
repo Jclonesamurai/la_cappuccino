0b505156ff84933c6ea13432a758a90cfc7e47d2
Jclonesamurai <un-petit-cafe@live.fr>
2014-12-30 23:27:45 +0100

    rajout du formulaire de contact nom prénom e-mail

--- contact.php
+++ contact.php
@@ -10,5 +10,21 @@
 		<?php 
 			include("en_tete.php");	
 		?>
+		<form action="traitement.php" method="post">
+		
+		<fieldset>
+				<legend>Vos Coordonnées </legend>
+				
+				<label for="nom">Nom : </label> 
+				<input type="text" name="nom" id="nom" autofocus /><br /><!--mets le curseur automatiquement à l'endroit où on le souhaite-->
+				<label for="prenom">Prénom</label>
+				<input type="text" name="prenom" id="prenom" required /><br /> 
+				
+				<label for="email">E-mail ?</label>
+				<input type="email" name="email" id="email" /> <br /> 
+			   
+			    
+			</form>
+		</fieldset>
     </body>
 </html>
